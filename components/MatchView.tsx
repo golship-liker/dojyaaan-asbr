ac87f6c08af9943e3581842a465d17bb2585cfc4
Aaron Andrews <aaron.andrews1996@gmail.com>
2023-03-02 19:46:04 -0600

    a lil bit

--- components/MatchView.tsx
+++ components/MatchView.tsx
@@ -1,17 +1,26 @@
 import { Box } from "@mui/material";
+import { YouTube } from '@mui/icons-material'
 import Link from "next/link";
 interface MatchViewProps {
-  ytLink: String;
-  timestamp: String;
-  player1: String;
-  player2: String;
+  ytLink: string;
+  timestamp: string;
+  player1: string;
+  player2: string;
 }
 
 const MatchView = ({ytLink, timestamp, player1, player2} : MatchViewProps): JSX.Element => {
 
   return(
-  <Box>
-    A BOX!!!
+  <Box flexDirection={"row"}>
+    <Box>
+      {player1}
+    </Box>
+    <Box>
+      {player2}
+    </Box>
+    <a href={ytLink}>
+      <YouTube/>
+    </a>
   </Box>)
 };
 export default MatchView;
